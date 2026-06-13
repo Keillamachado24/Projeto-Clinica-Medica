e1c05329d6718b296e1acf68692fdfddeaffe956
Nauannems <nauannesantosmarinho@email.com>
2026-06-13 17:50:27 -0300

    tirei uma variavel

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.projeto_clinica_medica {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.projeto_clinica_medica to javafx.fxml;
